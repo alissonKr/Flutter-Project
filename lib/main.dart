b02692a77f1456c12bf81fb95aff4d8dc82f818c
alissonKr <alissonkrugerc@gmail.com>
2023-03-06 13:50:10 -0300

    LinearProgressIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -37,43 +37,83 @@ class MyApp extends StatelessWidget {
 
 class task extends StatefulWidget {
   final String nome;
-  const task(this.nome,{Key? key}) : super(key: key);
+  const task(this.nome, {Key? key}) : super(key: key);
 
   @override
   State<task> createState() => _taskState();
 }
 
 class _taskState extends State<task> {
+  int nivel = 0;
   @override
   Widget build(BuildContext context) {
-    int nivel = 0;
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(child:
-      Stack(
-        children: [
-          Container(color: Colors.blue,height:140,),
-          Column(
-            children: [
-              Container(color:  Colors.white,height: 100,child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(color: Colors.black26, width: 72, height: 100,),
-                  Container(
-                      width: 200,
-                      child: Text(widget.nome, style: TextStyle(fontSize: 24,
-                      overflow: TextOverflow.ellipsis ),)),
-                  ElevatedButton(onPressed: (){nivel ++;
-                    print(nivel);}, child: Icon(Icons.arrow_drop_up))
-                ],),),
-              Text('Nivel $nivel', style: TextStyle(color: Colors.white, fontSize: 16),),
-            ],
-          )
-        ],
-      ),),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: TextStyle(
+                                fontSize: 24, overflow: TextOverflow.ellipsis),
+                          )),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                            });
+                            print(nivel);
+                          },
+                          child: Icon(Icons.arrow_drop_up))
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: nivel / 10,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        'Nivel $nivel',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
-
-
-

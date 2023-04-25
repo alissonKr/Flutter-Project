2fba9cd5efd5a199bd06027c22d7922e596507e8
alissonKr <alissonkrugerc@gmail.com>
2023-04-24 21:48:08 -0300

    new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:alura/screens/form_screen.dart';
 import 'package:alura/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }

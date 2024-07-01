145324bb617a11ad1771a389273fb869ece7a478
alissonkrugerc <alissonkrugerc@gmail.com>
2024-07-01 19:40:12 +0000

    creating a table

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -4,5 +4,15 @@ import 'package:path/path.dart';
 
 Future<Database> getDatabase() async{
   final String path = join( await getDatabasePath(), 'task.db');
-  return openDatabase(path, onCreate: (db, version){db.execute(tabela)}, version: 1,);
+  return openDatabase(path, onCreate: (db, version){db.execute(tableSql)}, version: 1,);
 }
+
+const String tableSql ='CREATE TABLE $_tablename('
+    '$_name TEXT, '
+    '$_difficulty INTEGER, '
+    '$_image TEXT)'; 
+
+const String _tablename = 'taskTabl';
+const String _name = 'name';
+const String _difficulty = 'difficulty';
+const String _image = 'image';
